1b2f3966a5fc334d3eedc809b0f54486bb4c1d48
Yago Toro molina <ytoro-mo@c1r7s2.42malaga.com>
2022-04-22 15:49:36 +0200

    Funcion putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,14 +6,75 @@
 /*   By: ytoro-mo <ytoro-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 13:48:35 by ytoro-mo          #+#    #+#             */
-/*   Updated: 2022/04/20 15:15:00 by ytoro-mo         ###   ########.fr       */
+/*   Updated: 2022/04/22 15:47:31 by ytoro-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int		ft_exp(int i);
+void	ft_numb_to_char(long nb, int pos_neg, int fb);
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	n = 0;
-	fd = 0;
+	char	number;
+	long	long_nb;
+
+	long_nb = n;
+	if (long_nb == 0)
+	{
+		number = '0';
+		write(fd, &number, 1);
+	}
+	else if (n < 0)
+	{
+		long_nb = long_nb * (-1);
+		ft_numb_to_char(long_nb, 0, fd);
+	}
+	else
+	{
+		ft_numb_to_char(long_nb, 1, fd);
+	}
+}
+
+int	ft_exp(int i)
+{
+	int	exp_10;
+
+	exp_10 = 1;
+	while (i > 0)
+	{
+		exp_10 = exp_10 * 10;
+		i--;
+	}
+	return (exp_10);
+}
+
+void	ft_numb_to_char(long nb, int pos_neg, int fd)
+{
+	int		i;
+	long	exp_nb;
+	long	nb_2;
+	char	number;
+
+	nb_2 = nb;
+	i = 0;
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		i++;
+	}
+	if (pos_neg == 0)
+	{
+		number = '-';
+		write(fd, &number, 1);
+	}
+	while (i > 0)
+	{
+		exp_nb = ft_exp(i - 1);
+		number = (nb_2 / exp_nb) + 48;
+		nb_2 = nb_2 - ((nb_2 / exp_nb) * exp_nb);
+		write(fd, &number, 1);
+		i--;
+	}
 }
